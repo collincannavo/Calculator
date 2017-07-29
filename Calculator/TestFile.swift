35ce8591665595480a050335f630705b3afe5918
Bop <collin.cannavo@gmail.com>
2017-07-29 14:59:10 -0600

    added new file

---
+++ Calculator/TestFile.swift
@@ -0,0 +1,23 @@
+//
+//  TestFile.swift
+//  Calculator
+//
+//  Created by Collin Cannavo on 7/29/17.
+//  Copyright © 2017 Collin Cannavo. All rights reserved.
+//
+
+import Foundation
+
+
+class TestFile {
+    
+    var firstName: String = ""
+    var lastName: String = ""
+    
+    func callName(_ name: String, _ lastName: String){
+        
+        
+        
+    }
+    
+}
